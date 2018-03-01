cce0a98acc26756de865f751db19c5605505555e
lhwlyd <yil623@ucsd.edu>
2018-03-01 10:25:35 -0800

    thirdparty API lab

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,14 +1,8 @@
-var data = {
-  rsvp: ['ixd@ucsd.edu']
-};
+
 
 /*
  * GET tasks page.
  */
-exports.adminView = function(req, res){
-  res.render('rsvp', data);
-};
-
 exports.addRSVP = function(req, res){
 	var rsvpEmail = req.body.rsvpEmail;
 
@@ -18,4 +12,3 @@ exports.addRSVP = function(req, res){
 
 	res.send(rsvpEmail);
 }
-
